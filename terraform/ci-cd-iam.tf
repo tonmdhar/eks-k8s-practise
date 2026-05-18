5891cd41c4f57c60d242e3d06c330e6095f0d6d4
Tonmoy Dhar <tonmdhar@amazon.com>
2026-05-18 22:27:44 +0530

    Added  HPA, ALB Ingress, CI/CD pipeline, PDB

---
+++ terraform/ci-cd-iam.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_user" "github_actions" {
+  name = "github-actions-deployer"
+
+  tags = {
+    Project = "eks-k8s-practice"
+    Purpose = "CI/CD pipeline"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "github_ecr" {
+  user       = aws_iam_user.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_user_policy_attachment" "github_eks" {
+  user       = aws_iam_user.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_access_key" "github_actions" {
+  user = aws_iam_user.github_actions.name
+}
+
+output "github_actions_access_key_id" {
+  value     = aws_iam_access_key.github_actions.id
+  sensitive = true
+}
+
+output "github_actions_secret_access_key" {
+  value     = aws_iam_access_key.github_actions.secret
+  sensitive = true
+}
